dff0febccd10829093ecfb48a9e54fcd84c27d6b
Viacheslav Yemets <viacheslav.emets@gmail.com>
2022-08-27 22:42:44 +0300

    get users data using async request and async dispatch with redux-toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
+import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "./hooks/redux";
-import {userSlice} from "./store/reducers/userSlice";
+import { fetchUsers } from "./store/reducers/ActionCreators";
+
 
 function App() {
-  const {count} = useAppSelector(state => state.userReducer);
-  const {increment} = userSlice.actions;
+  
   const dispatch = useAppDispatch();
+  const {users, isLoading, error} = useAppSelector(state => state.userReducer);
+
+  useEffect(()=>{
+    dispatch(fetchUsers())
+  }, [])
 
   return (
     <div>
-      {count}
-      <button onClick={()=>dispatch(increment(10))}>Increase</button>
+      {isLoading && <h1>Loading ...</h1>}
+      {error && <h1>{error}</h1>}
+      {JSON.stringify(users, null, 2)}
     </div>
   );
 }
